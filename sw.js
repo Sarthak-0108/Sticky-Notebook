4459f7ae39de2334b61cd5d019622c19e3d931a8
Sarthak-0108 <sarthakkumar0108@gmail.com>
2025-08-14 22:16:22 +0530

    feature successfully implemented

--- sw.js
+++ sw.js
@@ -1,13 +1,68 @@
+const cacheName = "PiNOte/v-1.0";
+const CACHE_FILES = [
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/notes.png",
+  "/android-icon-192x192.png",
+];
+
 self.addEventListener("install", (event) => {
   console.log("Service Worker installing...");
   // You can cache files here
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      console.log("caching files");
+      await cache.addAll(CACHE_FILES);
+    })()
+  );
 });
 
 self.addEventListener("activate", (event) => {
   console.log("Service Worker activated!");
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key === cacheName) {
+            return undefined;
+          }
+          return caches.delete(key);
+        })
+      )
+    )
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log("Fetching:", event.request.url);
-  // You can intercept requests and serve cached responses
+  // You can intercept requests and serve cached responsesis
+  const url = new URL(event.request.url);
+  if (
+    url.hostname.includes("google-analytics.com") ||
+    url.hostname.includes("cohere.ai") ||
+    url.hostname.includes("gemini.googleapis.com")
+  ) {
+    return;
+  }
+
+  event.respondWith(
+    (async () => {
+      try {
+        const r = await caches.match(event.request);
+        console.log("fetching request");
+
+        if (r) return r;
+        const response = await fetch(event.request);
+        console.log(response);
+
+        const cache = await caches.open(cacheName);
+        console.log("caching new resource");
+        await cache.put(event.request, response.clone());
+        return response;
+      } catch (error) {
+        console.log(error);
+      }
+    })()
+  );
 });
